3ae15c8a679fec93b7e6ae10f4201ddd31d04f7c
Dong-ik <ldigo@naver.com>
2025-11-26 13:50:23 +0900

    각 목록페이지 수정

--- src/pages/item/itemList/index.js
+++ src/pages/item/itemList/index.js
@@ -1,7 +1,29 @@
+import "./itemList.css";
+
 export const itemList = () => {
   return (
     <div>
       <h2>Item List Page</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>물품번호</th>
+            <th>물품이름</th>
+            <th>수량</th>
+            <th>보관창고</th>
+            <th>보관위치</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>001</td>
+            <td>노트북</td>
+            <td>10</td>
+            <td>창고 A</td>
+            <td>선반 3</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
--- src/pages/wherehouse/wherehouseList/index.js
+++ src/pages/wherehouse/wherehouseList/index.js
@@ -1,7 +1,29 @@
+import "./wherehouseList.css";
+
 export const wherehouseList = () => {
   return (
     <div>
       <h2>Wherehouse List Page</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>창고번호</th>
+            <th>창고이름</th>
+            <th>창고이미지</th>
+            <th>창고위치</th>
+            <th>창고 관리자</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>WH001</td>
+            <td>중앙창고</td>
+            <td></td>
+            <td>서울특별시 강남구</td>
+            <td>홍길동</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
