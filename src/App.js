9a2de9dcb63d6995d47a15d83207d7e55f6f81b9
Dong-ik <ldigo@naver.com>
2025-11-26 12:21:01 +0900

    하위페이지 경로 설정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Layout } from "./layouts/layout";
 import { Wherehouse } from "./pages/wherehouse";
+import { wherehouseList as WherehouseList } from "./pages/wherehouse/wherehouseList";
+import { wherehouseAdmin as WherehouseAdmin } from "./pages/wherehouse/wherehouseAdmin";
 import { Item } from "./pages/item";
+import { itemList as ItemList } from "./pages/item/itemList";
+import { itemAdmin as ItemAdmin } from "./pages/item/itemadmin";
 import { Home } from "./pages/home";
 
 function App() {
@@ -10,8 +14,12 @@ function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/wherehouse" element={<Wherehouse />} />
         <Route path="/item" element={<Item />} />
+        <Route path="/item/itemList" element={<ItemList />} />
+        <Route path="/item/itemadmin" element={<ItemAdmin />} />
+        <Route path="/wherehouse" element={<Wherehouse />} />
+        <Route path="/wherehouse/wherehouseList" element={<WherehouseList />} />
+        <Route path="/wherehouse/wherehouseAdmin" element={<WherehouseAdmin />} />
       </Routes>
     </Layout>
   );
--- src/entities/sidebar/index.js
+++ src/entities/sidebar/index.js
@@ -1,25 +1,48 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import "./sidebar.css";
 
-const Sidebar = () => (
-  <aside className="sidebar">
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/inventory">Inventory</a>
-        </li>
-        <li>
-          <a href="/orders">Orders</a>
-        </li>
-        <li>
-          <a href="/settings">Settings</a>
-        </li>
-      </ul>
-    </nav>
-  </aside>
-);
+const Sidebar = () => {
+  const location = useLocation();
+
+  const getMenuItems = () => {
+    if (location.pathname === "/") {
+      return [{ label: "Home", href: "/" }];
+    } else if (location.pathname.startsWith("/item")) {
+      return [
+        { label: "Item", href: "/item" },
+        { label: "Item List", href: "/item/itemList" },
+        { label: "Item Admin", href: "/item/itemadmin" },
+      ];
+    } else if (location.pathname.startsWith("/wherehouse")) {
+      return [
+        { label: "Wherehouse", href: "/wherehouse" },
+        { label: "Wherehouse List", href: "/wherehouse/wherehouseList" },
+        { label: "Wherehouse Admin", href: "/wherehouse/wherehouseAdmin" },
+      ];
+    }
+    return [
+      { label: "Home", href: "/" },
+      { label: "Item", href: "/item" },
+      { label: "Wherehouse", href: "/wherehouse" },
+    ];
+  };
+
+  const menuItems = getMenuItems();
+
+  return (
+    <aside className="sidebar">
+      <nav>
+        <ul>
+          {menuItems.map((item) => (
+            <li key={item.href}>
+              <a href={item.href}>{item.label}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+};
 
 export default Sidebar;
